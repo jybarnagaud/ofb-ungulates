b7875fe6fc292e770ccb298462ba2bb4ea341dcd
jybarnagaud <jeanyves.barnagaud@gmail.com>
2024-04-11 15:05:24 +0200

    charger les données simulation de matrices (sans intérêt)

--- m1_jacquemin_24/scripts/explore-herbivorie-initial.R
+++ m1_jacquemin_24/scripts/explore-herbivorie-initial.R
@@ -4,8 +4,6 @@
 # réunion du 11 avril 2024 - début stage
 #-----------------------------------------#
 
-setwd("m1_jacquemin_24")
-
 ## librairies ------------------------------------------
 
 library(readxl)
@@ -14,5 +12,22 @@ library(readxl)
 ## data ------------------------------------------------
 
 herbivorie <-
-  read_excel("data/bdd_propre_PF_RNCFS_WG.xlsx", sheet = "bdd_propre_PF_RNCFS_WG")
+  as.data.frame(read_excel("m1_jacquemin_24/data/bdd_propre_PF_RNCFS_WG.xlsx"))
 
+herbivorie$code_territoire <- factor(herbivorie$code_territoire)
+
+pres <- matrix(rbinom(300,1,0.4),nrow = 30, ncol = 10)
+abr <- pres
+abr2 <- matrix(sapply(abr,FUN = function(x) {ifelse(x>0,rbinom(1,1,0.3),0)}), nrow = 30)
+
+colnames(pres) = paste("A", 1:10,sep="")
+colnames(abr2) = paste("A", 1:10,sep="")
+
+library(ade4)
+
+afc.pres <- dudi.coa(pres,scannf= F, nf = 2)
+afc.abr <- dudi.coa(abr2,scannf = F, nf = 2)
+
+co.afc <- coinertia(afc.pres,afc.abr)
+
+proc <- procuste(pres,abr2)
