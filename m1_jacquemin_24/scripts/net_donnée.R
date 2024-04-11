cd88eea6cb81d7ed82d0344f2d7fe264407ce02f
Anaïs Jacquemin <jacquemin.anais14@gmail.com>
2024-04-11 16:52:29 +0200

    Ajout col guilde

--- m1_jacquemin_24/scripts/net_donnée.R
+++ m1_jacquemin_24/scripts/net_donnée.R
@@ -6,6 +6,8 @@ setwd("m1_jacquemin_24")
 ## librairies ------------------------------------------
 
 library(readxl)
+library(openxlsx)
+library(dplyr)
 
 
 ## Importation data ------------------------------------------------
@@ -21,14 +23,33 @@ print(liste_essence)
 
 ## MAJ -------------------------------------------------------------------------
 
-clean_2 <- clean_1  # -> Creation de clean_2 avec les valeur de clean_1
-clean_2$essence <- toupper(clean_1$essence)  ## Tout en maj pour essence
+bdd_propre_PF_RNCFS_WG_Clean <- clean_1  # -> Creation de clean_2 avec les valeur de clean_1
+bdd_propre_PF_RNCFS_WG_Clean$essence <- toupper(clean_1$essence)  ## Tout en maj pour essence
 
   ## Liste essence
-liste_essence_2 <- sort(unique(clean_2$essence))
+liste_essence_2 <- sort(unique(bdd_propre_PF_RNCFS_WG_Clean$essence))
 print(liste_essence_2)
+## -> C'est ok, tout en Maj
+
+## Sauvegarde xlxs
+
+chemin_fichier <- "C:/Users/anais/Documents/ofb_m1/ofb-ungulates/m1_jacquemin_24/data/bdd_propre_PF_RNCFS_WG_Clean.xlsx"
+write.xlsx(bdd_propre_PF_RNCFS_WG_Clean, chemin_fichier, rowNames = FALSE)
 
 
+## Nouv colonne guilde resineux/feuillus/semi-ligneux -------------------------------------------------------
+
+plus_guilde <- bdd_propre_PF_RNCFS_WG_Clean
+
+plus_guilde <- plus_guilde %>%
+  mutate(guilde = case_when(
+    essence %in% c("ALISIER BLANC", "ALISIER DE MOUGEOT") ~ "FEUILLUS",
+    essence == "SAPIN PECTINE" ~ "RESINEUX",
+    essence == "RONCES" ~ "SEMI_LIGNEUX",
+    TRUE ~ NA_character_  
+  ))
+
+############## NOISETIER = COUDRIER
 
 
 
