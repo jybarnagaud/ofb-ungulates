adcff24ad0833e1e85601114015b6e95ee6b72af
jybarnagaud <jeanyves.barnagaud@gmail.com>
2024-04-12 14:09:49 +0200

    add AFC sur les occurrences

--- m1_jacquemin_24/scripts/explore-herbivorie-initial.R
+++ m1_jacquemin_24/scripts/explore-herbivorie-initial.R
@@ -19,16 +19,34 @@ library(factoextra)
 herbivorie <-
   as.data.frame(read_excel("m1_jacquemin_24/data/bdd_propre_PF_RNCFS_WG_clean.xlsx"))
 
+# table par genres
+
+
+herbivorie.genre <-
+  aggregate(
+    herbivorie[, c("presence", "consommation")],
+    by = list(
+      herbivorie$genre,
+      herbivorie$annee,
+      herbivorie$placette,
+      herbivorie$guilde,
+      herbivorie$type_feuillage
+    ),
+    FUN = "max"
+  )
+colnames(herbivorie.genre) <- c("genre","annee","placette","guilde","type","presence","consommation")
+
 ## exploration ------------------------------------------
 
 # Subset du tab avec que de la consomation 
 
-tab_conso <- herbivorie %>%
+tab_conso <- herbivorie.genre %>%
   filter(consommation == 1)
+
 
 # Test de trucs
 
-donnees <- data.frame(Classe_d_essence = names(table(tab_conso$essence)),
+donnees <- data.frame(genre = names(table(tab_conso$genre)),
                       Nombre_d_occurrences = as.vector(table(tab_conso$essence)))
 
 # Créer le diagramme à barres
@@ -44,19 +62,41 @@ print(diagramme)
 
 # reshape données de présence 
 
-h.pres <- aggregate(herbivorie$presence, by = list(herbivorie$placette, herbivorie$essence), FUN = "max")
+h.pres <-
+  aggregate(
+    herbivorie.genre$presence,
+    by = list(herbivorie.genre$placette, herbivorie.genre$genre),
+    FUN = "max"
+  )
 h.pres.w <- h.pres %>%
-  pivot_wider(names_from = Group.2, values_from = x, values_fill = 0) %>% 
+  pivot_wider(names_from = Group.2,
+              values_from = x,
+              values_fill = 0) %>%
   as.data.frame()
 rownames(h.pres.w) <- h.pres.w$Group.1
-h.pres.w2 <- h.pres.w[,-1]
+h.pres.w2 <- h.pres.w[, -1]
 
 # do AFC 
 
-h.pres.w3 <- h.pres.w2[,!names(h.pres.w2)%in%c("CORONILLE ARBRISSEAU")]
+h.pres.w3 <- h.pres.w2[,!names(h.pres.w2)%in%c("berberis")]
 afc.pres <- dudi.coa(h.pres.w3, scannf = F, nf = 10)
 screeplot(afc.pres)
 cumsum(afc.pres$eig)/sum(afc.pres$eig)
 
 s.label(afc.pres$li,plabels = list(cex = 0.5))
+s.label(afc.pres$co,plabels = list(cex = 0.8, col = "goldenrod", boxes = list(draw = T, alpha = 0.8)),add = T)
 
+# add abroutissement
+
+conso.plac <- unique(herbivorie.genre[,c("consommation","placette","annee")])
+
+abrou.plac <-
+  aggregate(
+    conso.plac$consommation,
+    by = list(conso.plac$placette),
+    FUN = sum
+  )
+
+colnames(abrou.plac) <- c("placette","nb.yr.abrou")
+
+s.value(afc.pres$li,z = abrou.plac$nb.yr.abrou , method = "color", col = colorRampPalette(c("yellow", "blue"))(3))
